1845f779916ed9c688cad6829c88c18838492c3b
Kenneth <kennethnym@outlook.com>
2024-03-07 17:28:09 +0000

    Add bare write methods

--- include/nanopack/writer.hxx
+++ include/nanopack/writer.hxx
@@ -9,6 +9,30 @@
 
 namespace NanoPack {
 
+void write_type_id(TypeId type_id, int offset, std::vector<uint8_t> &buf);
+
+void write_field_size(int field_number, int32_t size, int offset,
+					  std::vector<uint8_t> &buf);
+
+void append_int8(int8_t num, std::vector<uint8_t> &buf);
+void append_uint8(uint8_t num, std::vector<uint8_t> &buf);
+
+void append_int32(int32_t num, std::vector<uint8_t> &buf);
+void append_uint32(uint32_t num, std::vector<uint8_t> &buf);
+
+void append_int64(int64_t num, std::vector<uint8_t> &buf);
+void append_uint64(uint16_t num, std::vector<uint8_t> &buf);
+
+void append_string(const std::string &str, std::vector<uint8_t> &buf);
+void append_string(const std::string_view &string_view,
+				   std::vector<uint8_t> &buf);
+
+void append_bytes(const std::vector<uint8_t> &bytes, std::vector<uint8_t> &buf);
+
+void append_bool(bool b, std::vector<uint8_t> &buf);
+
+void append_double(double d, std::vector<uint8_t> &buf);
+
 class Writer {
   private:
 	int offset;
--- src/writer.cxx
+++ src/writer.cxx
@@ -1,5 +1,90 @@
 #include <nanopack/writer.hxx>
 
+void NanoPack::write_type_id(TypeId type_id, int offset,
+							 std::vector<uint8_t> &buf) {
+	buf[offset] = type_id & 0x000000FF;
+	buf[offset + 1] = (type_id & 0x0000FF00) >> 8;
+	buf[offset + 2] = (type_id & 0x00FF0000) >> 16;
+	buf[offset + 3] = (type_id & 0xFF000000) >> 24;
+}
+
+void NanoPack::write_field_size(const int field_number, const int32_t size,
+								const int offset, std::vector<uint8_t> &buf) {
+	const int p = offset + 4 * (field_number + 1);
+	buf[p] = size & 0x000000FF;
+	buf[p + 1] = (size & 0x0000FF00) >> 8;
+	buf[p + 2] = (size & 0x00FF0000) >> 16;
+	buf[p + 3] = (size & 0xFF000000) >> 24;
+}
+
+void NanoPack::append_int8(const int8_t num, std::vector<uint8_t> &buf) {
+	buf.emplace_back(num);
+}
+
+void NanoPack::append_uint8(const uint8_t num, std::vector<uint8_t> &buf) {
+	buf.emplace_back(num);
+}
+
+void NanoPack::append_int32(const int32_t num, std::vector<uint8_t> &buf) {
+	buf.emplace_back(num & 0x000000FF);
+	buf.emplace_back((num & 0x0000FF00) >> 8);
+	buf.emplace_back((num & 0x00FF0000) >> 16);
+	buf.emplace_back((num & 0xFF000000) >> 24);
+}
+
+void NanoPack::append_uint32(uint32_t num, std::vector<uint8_t> &buf) {
+	buf.emplace_back(num & 0x000000FF);
+	buf.emplace_back((num & 0x0000FF00) >> 8);
+	buf.emplace_back((num & 0x00FF0000) >> 16);
+	buf.emplace_back((num & 0xFF000000) >> 24);
+}
+
+void NanoPack::append_int64(int64_t num, std::vector<uint8_t> &buf) {
+	buf.emplace_back(num & 0x00000000000000FF);
+	buf.emplace_back((num & 0x000000000000FF00) >> 8);
+	buf.emplace_back((num & 0x0000000000FF0000) >> 16);
+	buf.emplace_back((num & 0x00000000FF000000) >> 24);
+	buf.emplace_back((num & 0x000000FF00000000) >> 32);
+	buf.emplace_back((num & 0x0000FF0000000000) >> 40);
+	buf.emplace_back((num & 0x00FF000000000000) >> 48);
+	buf.emplace_back((num & 0xFF00000000000000) >> 56);
+}
+
+void NanoPack::append_uint64(uint16_t num, std::vector<uint8_t> &buf) {
+	buf.emplace_back(num & 0x00000000000000FF);
+	buf.emplace_back((num & 0x000000000000FF00) >> 8);
+	buf.emplace_back((num & 0x0000000000FF0000) >> 16);
+	buf.emplace_back((num & 0x00000000FF000000) >> 24);
+	buf.emplace_back((num & 0x000000FF00000000) >> 32);
+	buf.emplace_back((num & 0x0000FF0000000000) >> 40);
+	buf.emplace_back((num & 0x00FF000000000000) >> 48);
+	buf.emplace_back((num & 0xFF00000000000000) >> 56);
+}
+
+void NanoPack::append_string(const std::string &str,
+							 std::vector<uint8_t> &buf) {
+	buf.insert(buf.end(), str.begin(), str.end());
+}
+
+void NanoPack::append_string(const std::string_view &string_view,
+							 std::vector<uint8_t> &buf) {
+	buf.insert(buf.end(), string_view.begin(), string_view.end());
+}
+
+void NanoPack::append_bytes(const std::vector<uint8_t> &bytes,
+							std::vector<uint8_t> &buf) {
+	buf.insert(buf.end(), bytes.begin(), bytes.end());
+}
+
+void NanoPack::append_bool(bool b, std::vector<uint8_t> &buf) {
+	buf.emplace_back(b ? 1 : 0);
+}
+
+void NanoPack::append_double(double d, std::vector<uint8_t> &buf) {
+	auto *bytes = reinterpret_cast<uint8_t *>(&d);
+	buf.insert(buf.end(), bytes, bytes + sizeof(double));
+}
+
 NanoPack::Writer::Writer(std::vector<uint8_t> *buffer) : Writer(buffer, 0) {}
 
 NanoPack::Writer::Writer(std::vector<uint8_t> *buffer, const int offset)
