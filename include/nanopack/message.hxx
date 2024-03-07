054baa4c796af0e248470403288443584e10a60b
Kenneth <kennethnym@outlook.com>
2024-03-07 22:07:13 +0000

    Make write_to return the no. of bytes written

--- include/nanopack/message.hxx
+++ include/nanopack/message.hxx
@@ -16,7 +16,7 @@ class Message {
 
 	[[nodiscard]] virtual int header_size() const = 0;
 
-	virtual void write_to(std::vector<uint8_t> &buf, int offset) const = 0;
+	virtual size_t write_to(std::vector<uint8_t> &buf, int offset) const = 0;
 
 	[[nodiscard]] virtual std::vector<uint8_t> data() const = 0;
 };
