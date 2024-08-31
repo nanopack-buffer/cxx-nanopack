805d729b5f66fe4ebe54e2ae760dc6e96ff7b5f9
Kenneth <kennethnym@outlook.com>
2024-08-31 22:57:44 +0100

    chore: add flake.nix

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  description = "C++ support library for NanoPack";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?tag=24.05";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+
+      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+    in
+    {
+      packages = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        rec {
+          libnanopack = pkgs.stdenv.mkDerivation {
+            pname = "libnanopack";
+            version = "0.1.0";
+            src = ./.;
+            nativeBuildInputs = [
+              pkgs.cmake
+            ];
+          };
+
+          default = libnanopack;
+        }
+      );
+
+      devShells = forAllSystems (system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.cmake
+              pkgs.clang-tools
+            ];
+          };
+        }
+      );
+    };
+}
